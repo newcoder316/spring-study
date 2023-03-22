99c505f258aa9c4bcb710cc416afcd6bc8ce5e69
taku <derenzhuo@gmail.com>
2023-03-22 16:46:18 +0900

    springboot demo initial commit

---
+++ src/main/java/com/springboot/taku/demobytaku/DemobytakuApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.taku.demobytaku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobytakuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobytakuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/taku/demobytaku/DemobytakuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.taku.demobytaku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobytakuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
